1b40e5dbfedcacbd51b1b917a889c09d5124bb73
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-14 15:38:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,15 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  block_return_values = []
+  return
   while i < collection.length
-    block_return_values << yield(collection[i])
-    i += 1
-  end
-
-  if block_return_values.include?(false)
-    false
-  else
-    true
+    yield(collection[i])
+    i = i + 1
   end
 end
